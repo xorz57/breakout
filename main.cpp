5a2b7e080bb4c30cec1cc1215ff79b06cfb2d6e6
Georgios Fotopoulos <xorz57@gmail.com>
2026-04-02 23:37:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
+#include <cstdlib>
+
 #include "spdlog/spdlog.h"
 
+// clang-format off
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+// clang-format on
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  static_cast<void>(window);
+  glViewport(0, 0, width, height);
+}
+
 int main() {
-    spdlog::info("Hello World!");
-    return 0;
+  if (!glfwInit()) {
+    spdlog::error("Failed to initialize GLFW");
+    return EXIT_FAILURE;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(1280, 720, "Breakout", nullptr, nullptr);
+  if (!window) {
+    spdlog::error("Failed to create GLFW window");
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+    spdlog::error("Failed to initialize GLAD");
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  glViewport(0, 0, 1280, 720);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return EXIT_SUCCESS;
 }
